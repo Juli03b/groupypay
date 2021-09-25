4ab22c6dd9818ccf12487e22b6b24b874da30b64
Julio Barrios <julio.junior02@gmail.com>
2021-09-25 18:16:15 -0500

    Update user route in app

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+from json_validation import validate_json
 from os import environ
 from flask import Flask, request
 from models.Users import Users
 from flask.json import jsonify
 from models.main import connect_db, db
 from sqlalchemy.exc import IntegrityError
-from jsonschema import validate, ValidationError, SchemaError
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
-import json
+from email_validator import validate_email, EmailNotValidError
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_ECHO'] = True
@@ -20,37 +20,36 @@ jwt = JWTManager(app)
 connect_db(app)
 db.create_all()
 
-with open("./json_schemas/user_schema.json", "r") as js:
-    user_schema = json.load(js)
 @app.get("/")
 def home():
     return "NONE"
 
 @app.post("/users")
-def signUp():
-    try:
-        valid_input = validate(request.json, user_schema)
-        return jsonify(valid_input)
-    
-    except (ValidationError, SchemaError) as e:
-        return jsonify(dir(e), str(e.message))
-        
-    user = Users.signUp(
-        first_name=request.json.get("first_name"), 
-        last_name=request.json.get("last_name"), 
-        email=request.json.get("email"), 
-        password=request.json.get("password"), 
-        phone_number=request.json.get("phone_number"))
+def sign_up(): 
+    valid_json, msg = validate_json(request.json, "user_schema")
 
+    # Check if JSON request is valid, return error and HTTP code accordingly
+    if not valid_json:
+        return jsonify(error=msg), 400
+    try:
+        valid_email = validate_email(request.json["email"])
+        print(valid_email.email)
+    except EmailNotValidError as e:
+        return jsonify(error=dict(message=str(e), cause="email")), 400
+    # Create user
+    user, warning = Users.sign_up(**request.json)
+
+    # Attempt making entry to db, return error and HTTP code if failed
     try:
         db.session.commit()
     except IntegrityError as e:
-        print("EEEEEEEEEEEEEEEE", e)
         db.session.rollback()
-        [message] = e.orig.args
-        error_code = e.orig.pgcode
 
-        return jsonify(message, error_code), 409
-    print(user)
+        [message] = e.orig.args
+
+        return jsonify(error=dict(message=message, pg_code=e.orig.pgcode)), 409
+
+    # Create jwt token containing user's email
     access_token = create_access_token(user.email)
-    return jsonify(access_token=access_token)
+
+    return jsonify(message="Sign up successful", warning=warning, access_token=access_token)
