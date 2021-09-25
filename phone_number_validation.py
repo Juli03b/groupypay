9c82384403a94aa05f68115de7be76ee16cb3cfd
Julio Barrios <julio.junior02@gmail.com>
2021-09-25 18:15:51 -0500

    Add function to validate phone numbers

---
+++ phone_number_validation.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+import phonenumbers
+from phonenumbers.phonenumberutil import NumberParseException
+
+def validate_phone_number(phone_number: str) -> Tuple:
+    error_msg = "Phone number is invalid, none saved"
+    try:
+        phone_number = phonenumbers.parse(phone_number)
+
+        if phonenumbers.is_valid_number(phone_number):
+            # If number is valid and parsable, format it and return it
+            return phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164), None
+
+    except NumberParseException as e:
+        error_msg = e.args[0]
+    
+    return None, error_msg
+    
