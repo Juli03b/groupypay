4b541e70eb60e203e57b3b3962333068aafd69b9
Julio Barrios <julio.junior02@gmail.com>
2021-09-26 14:39:21 -0500

    Update app to extract user route logic

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from json_validation import validate_json
 from os import environ
-from flask import Flask, request
-from models.Users import Users
-from flask.json import jsonify
+from flask import Flask
 from models.main import connect_db, db
-from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
-from email_validator import validate_email, EmailNotValidError
+from user import sign_up
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_ECHO'] = True
@@ -25,31 +22,5 @@ def home():
     return "NONE"
 
 @app.post("/users")
-def sign_up(): 
-    valid_json, msg = validate_json(request.json, "user_schema")
-
-    # Check if JSON request is valid, return error and HTTP code accordingly
-    if not valid_json:
-        return jsonify(error=msg), 400
-    try:
-        valid_email = validate_email(request.json["email"])
-        print(valid_email.email)
-    except EmailNotValidError as e:
-        return jsonify(error=dict(message=str(e), cause="email")), 400
-    # Create user
-    user, warning = Users.sign_up(**request.json)
-
-    # Attempt making entry to db, return error and HTTP code if failed
-    try:
-        db.session.commit()
-    except IntegrityError as e:
-        db.session.rollback()
-
-        [message] = e.orig.args
-
-        return jsonify(error=dict(message=message, pg_code=e.orig.pgcode)), 409
-
-    # Create jwt token containing user's email
-    access_token = create_access_token(user.email)
-
-    return jsonify(message="Sign up successful", warning=warning, access_token=access_token)
+def _():
+    return sign_up()
---
+++ user.py
@@ -0,0 +1,52 @@
+from flask import request
+from flask.json import jsonify
+from json_validation import validate_json
+from sqlalchemy.exc import IntegrityError
+from app import db, create_access_token
+from models.Users import Users
+from phone_number_validation import validate_phone_number
+from email_validator import EmailNotValidError, validate_email
+
+def sign_up(**all): 
+    valid_json, msg = validate_json(request.json, "user_schema")
+
+    # Extra information to be returned, can be empty
+    warning = None
+
+    # Parse and validate phone number if exists, set to false if parsing or validation fails
+    if phone_number := request.json.get("phone_number"):
+        formated_number, msg = validate_phone_number(phone_number)
+
+        if formated_number:
+            request.json["phone_number"] = formated_number
+        else:
+            warning = dict(message=msg, cause="phone_number")
+    else:
+        warning = dict(message="Phone number was not provided, none saved", cause="phone_number")
+
+    # Check if JSON request is valid, return error and HTTP code accordingly
+    if not valid_json:
+        return jsonify(error=msg), 400
+    try:
+        valid_email = validate_email(request.json["email"])
+        print(valid_email.email)
+    except EmailNotValidError as e:
+        return jsonify(error=dict(message=str(e), cause="email")), 400
+        
+    # Create user
+    user = Users.sign_up(**request.json)
+
+    # Attempt making entry to db, return error and HTTP code if failed
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        db.session.rollback()
+
+        [message] = e.orig.args
+
+        return jsonify(error=dict(message=message, pg_code=e.orig.pgcode)), 409
+
+    # Create jwt token containing user's email
+    access_token = create_access_token(user.email)
+
+    return jsonify(message="Sign up successful", warning=warning, access_token=access_token)
