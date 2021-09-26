af12c25582b96863ea02f9d70481ff24168f2a4f
Julio Barrios <julio.junior02@gmail.com>
2021-09-26 14:43:29 -0500

    Update phone number validation

--- phone_number_validation.py
+++ phone_number_validation.py
@@ -4,13 +4,15 @@ from phonenumbers.phonenumberutil import NumberParseException
 
 def validate_phone_number(phone_number: str) -> Tuple:
     error_msg = "Phone number is invalid, none saved"
+    
     try:
         phone_number = phonenumbers.parse(phone_number)
 
         if phonenumbers.is_valid_number(phone_number):
             # If number is valid and parsable, format it and return it
             return phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164), None
-
+        else:
+            print(phone_number)
     except NumberParseException as e:
         error_msg = e.args[0]
     
