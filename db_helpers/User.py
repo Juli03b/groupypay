06465e5a6275d61c7b1af706397841ddae5dddc9
Julio Barrios <julio.junior02@gmail.com>
2021-09-27 13:14:22 -0500

    Add db_helpers directory and User helper. DB helpers are a layer of abstraction on top of SQLAlchemy

---
+++ db_helpers/User.py
@@ -0,0 +1,36 @@
+from sqlalchemy.exc import IntegrityError
+from models.Users import Users
+from app import db
+
+class User:
+    """Class for logic abstraction from views"""
+    def __init__(self, email=None):
+        if email:
+            user = Users.query.filter_by(email=email).first()
+
+            self.email = email
+            self.first_name = user.first_name
+            self.last_name = user.last_name
+            self.email = user.email
+            self.phone_number = user.phone_number
+
+    @classmethod
+    def sign_up(cls, **validated_json):
+
+        # Create user
+        user = Users.sign_up(**validated_json)
+
+        # Attempt making entry to db. If failed, return error with message and pg code
+        try:
+            db.session.commit()
+        except IntegrityError as e:
+
+            db.session.rollback()
+            [message] = e.orig.args
+
+            raise Exception(dict(error=dict(message=message, pg_code=e.orig.pgcode)))
+
+        del user.password
+
+        return cls(**user)
+
