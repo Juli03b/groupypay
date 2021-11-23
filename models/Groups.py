e5d2b166320daad9c453353fca29cfeba6e43340
Julio Barrios <julio.junior02@gmail.com>
2021-11-22 19:18:42 -0600

    Add changes to Groups model for relationship with group Payments_Model

--- models/Groups.py
+++ models/Groups.py
@@ -4,40 +4,30 @@ from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 
-class Users(db.Model):
-    __tablename__ = "users"
+class Groups(db.Model):
+    __tablename__ = "groups"
 
     id = db.Column(
-        db.Integer, 
-        primary_key=True, 
+        db.Integer,
+        primary_key=True,
         autoincrement=True)
     
     name = db.Column(
-        db.String(55),
+        db.String(30),
         nullable=False
     )
 
-    email = db.Column(
-        db.String(127),
-        nullable=False,
-        unique=True
+    description = db.Column(
+        db.String(200),
+        nullable=True
     )
-
-    password = db.Column(
-        db.String(),
-        nullable=False
-    )
-
-    phone_number = db.Column(
-        db.String(),
-        nullable=True,
-        unique=True
-    )
-
+    
     created_on = db.Column(
         db.DateTime(timezone=True),
         default=now()
     )
 
+    payments = db.relationship("Group_Payments", backref="groups", passive_deletes=True)
+    
     def __repr__(self):
-        return f'<User id={self.id} first_name={self.first_name} last_name={self.last_name} email={self.email} phone_number={self.phone_number}>'
+        return f'<Group id={self.id} first_name={self.name} last_name={self.last_name} email={self.email} phone_number={self.created_on}>'
