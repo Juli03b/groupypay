f863c50544ecd5d8002c98888f542a5558af80b7
Julio Barrios <julio.junior02@gmail.com>
2021-11-23 19:05:49 -0600

    Fix imports and compatibility

--- models/Group_Payments.py
+++ models/Group_Payments.py
@@ -1,17 +1,20 @@
 from models.main import db
 from sqlalchemy.sql.functions import now
+from models.Member_Payments import Member_Payments
 
 class Group_Payments(db.Model):
     __tablename__ = "group_payments"
 
     id = db.Column(
-        db.Integer,
+        db.Integer(),
         primary_key=True,
-        autoincrement=True)
+        autoincrement=True,
+        unique=True
+    )
     
     group_id = db.Column(
-        db.Integer,
-        db.ForeignKey("group.id", ondelete="CASCADE"),
+        db.Integer(),
+        db.ForeignKey("groups.id", ondelete="CASCADE"),
         primary_key=True
     )
 
@@ -25,7 +28,8 @@ class Group_Payments(db.Model):
         default=now()
     )
     
-    payments = db.relationship("Member_Payments", backref="group_payments", passive_deletes=True)
+    group = db.relationship("Groups", backref="group_payments", passive_deletes=True)
+    payments = db.relationship("Member_Payments", backref="group_members_payments", passive_deletes=True)
 
     def __repr__(self):
         return f'<Group_Payments id={self.id} group_id={self.group_id} total_amount={self.total_amount} created_on={self.created_on}>'
