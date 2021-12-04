06e37354daccb0d54239b80d90d3a9b933113253
Julio Barrios <julio.junior02@gmail.com>
2021-12-03 21:10:25 -0600

    Fix query

--- db_helpers/Group_Payment.py
+++ db_helpers/Group_Payment.py
@@ -15,27 +15,34 @@ class Group_Payment:
         self.total_amount = payment.total_amount
         self.member_payments = payment.member_payments
         self.created_on = payment.created_on
+        self.key = (self.id, self.group_id)
     
     def __repr__(self) -> str:
         return f"<Group_Payment id={self.id} group_id={self.group_id} name={self.name} total_amount={self.total_amount} member_payments={self.member_payments} created_on={self.created_on}>"
     
     @classmethod
-    def get_by_id(cls, id: str):
-        """Return a user using an id"""
-        
-        payment: Group_Payments = Group_Payments.query.filter_by(id=id)
+    def get_by_id(cls, id: int, group_id: int):
+        """Return a user using id and group_id"""
+        payment: Group_Payments = Group_Payments.query.get((id, group_id))
         
         return cls(payment)
  
     def edit(self, name: str=None, total_amount=None) -> None:
         """Edit payment using id"""
-        payment: Group_Payments = Group_Payments.query.filter_by(id=id).first()
+        payment: Group_Payments = Group_Payments.query.filter_by(id=self.id, group_id=self.group_id).first()
         payment.name = self.name = name or payment.name
         payment.total_amount = self.total_amount = total_amount or payment.total_amount
 
-        db.session.commit()
-        
+        try:
+            db.session.commit()
+        except IntegrityError as error:
+            db.session.rollback()
+            [message] = error.orig.args
+
+            raise Bad_Request(message, "Database error", pgcode=error.orig.pgcode) from error
+
     def delete(self) -> None:
         """Delete payment using id"""
         
-        Group_Payments.query.filter_by(id=self.id).delete()
+        Group_Payments.query.filter_by(id=self.id, group_id=self.group_id).delete()
+        db.session.commit()
