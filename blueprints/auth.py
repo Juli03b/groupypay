ccf321bf3170807bf8e39b316bc91b676d2826fe
Julio Barrios <julio.junior02@gmail.com>
2021-12-06 16:58:52 -0600

    Move sign up view

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -16,3 +16,28 @@ auth_blueprint = Blueprint("auth", __name__)
 def get_token():
     """Return token given username and password in request body"""
     return request.json
+@auth_blueprint.post("/sign-up", strict_slashes=True)
+def sign_up():
+    """Sign up view. Validate json request body and create user if valid"""
+
+    # Check if JSON request is valid, return error and HTTP code accordingly
+    validate_json(request.json, "auth")
+
+    # Parse and validate phone number if exists
+    if phone_number := request.json.get("phone_number"):
+        formated_number = validate_phone_number(phone_number)
+        request.json["phone_number"] = formated_number
+
+    try:
+        validate_email(request.json.get("email"))
+    except EmailNotValidError as error:
+        raise Bad_Request(message=str(error), cause="email") from error
+
+    user = User.sign_up(**request.json)
+    access_token = create_access_token(user.email)
+    warning = dict(message="Phone number was not provided, none saved", cause="phone_number")
+
+    return jsonify(
+        message="Sign up successful",
+        warning=warning if not request.json.get("phone_number") else None,
+        access_token=access_token), 201
--- blueprints/users.py
+++ blueprints/users.py
@@ -1,4 +1,4 @@
-"Blueprint for users views"
+"""Blueprint for users views"""
 
 from flask_jwt_extended import create_access_token
 from flask import request
@@ -8,35 +8,9 @@ from email_validator import EmailNotValidError, validate_email
 from json_validation import validate_json
 from phone_number_validation import validate_phone_number
 from db_helpers.User import User
-from exceptions.BadRequest import BadRequest
+from exceptions.Bad_Request import Bad_Request
 
 users_blueprint = Blueprint("users", __name__)
-
-@users_blueprint.post("/", strict_slashes=False)
-def sign_up():
-    """Sign up view. Validate json request body and create user if valid"""
-
-    # Check if JSON request is valid, return error and HTTP code accordingly
-    validate_json(request.json, "user_schema")
-
-    # Parse and validate phone number if exists
-    if phone_number := request.json.get("phone_number"):
-        formated_number = validate_phone_number(phone_number)
-        request.json["phone_number"] = formated_number
-
-    try:
-        validate_email(request.json.get("email"))
-    except EmailNotValidError as error:
-        raise BadRequest(message=str(error), cause="email") from error
-
-    user = User.sign_up(**request.json)
-    access_token = create_access_token(user.email)
-    warning = dict(message="Phone number was not provided, none saved", cause="phone_number")
-
-    return jsonify(
-        message="Sign up successful",
-        warning=warning if not request.json.get("phone_number") else None,
-        access_token=access_token), 201
 
 @users_blueprint.get("/<id>", strict_slashes=False)
 def get_user(id):
