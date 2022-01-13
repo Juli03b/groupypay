0e941774ed3f87754ed61f37ad1371e6030c1f28
Julio Barrios <julio.junior02@gmail.com>
2022-01-13 16:29:03 -0600

    Change get and patch user methods to use email instead of password

--- blueprints/users.py
+++ blueprints/users.py
@@ -1,29 +1,29 @@
 """Blueprint for users views"""
 
-from flask_jwt_extended import create_access_token
 from flask import request
 from flask.blueprints import Blueprint
 from flask.json import jsonify
 from email_validator import EmailNotValidError, validate_email
+from blueprints.helper import require_same_user_as_email
 from json_validation import validate_json
-from phone_number_validation import validate_phone_number
 from db_helpers.User import User
 from exceptions.Bad_Request import Bad_Request
 
 users_blueprint = Blueprint("users", __name__)
 
-@users_blueprint.get("/<id>", strict_slashes=False)
-def get_user(id: int):
-    """Get users view. Get user information."""
-    
-    user = User.get_by_id(id)
+@users_blueprint.get("/<email>", strict_slashes=False)
+def get_user(email: str):
+    """Get user information"""
+    user = User.get_by_email(email)
+    del user.password
     return jsonify(user.__dict__)
 
-@users_blueprint.patch("/<id>", strict_slashes=False)
-def patch_user(id: int):
+@users_blueprint.patch("/<email>", strict_slashes=False)
+@require_same_user_as_email
+def patch_user(email: str):
     """Patch user"""
     
-    user = User.get_by_id(id)
+    user = User.get_by_email(email)
     
     # Validate json
     validate_json(request.json, "user_patch")
@@ -35,10 +35,11 @@ def patch_user(id: int):
         except EmailNotValidError as error:
             raise Bad_Request(message=str(error), cause="email") from error
     user.edit(request.json.get("name"), request.json.get("email"), request.json.get("phone_number"), request.json.get("password"))
-    
-    return jsonify(message="Changes were made succesfully"), 200
+    del user.password
+    return jsonify(user.__dict__), 200
 
 @users_blueprint.post("/<id>/groups", strict_slashes=False)
+@require_same_user_as_email
 def make_group(id):
     """Make a group"""
     
