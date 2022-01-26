07777ffa6fee7916a90ad33bb1aeb1607bba31b6
Julio Barrios <julio.junior02@gmail.com>
2022-01-25 18:29:35 -0600

    Change auth requirements

--- blueprints/helper.py
+++ blueprints/helper.py
@@ -3,16 +3,20 @@ from flask import request
 from db_helpers.Group import Group
 from exceptions.Unauthorized import Unauthorized
 from db_helpers.User import User
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 def require_token():
+    print("req token: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", dir(request), request.get_json)
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!REQUIREDD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     if not request.json.get("token") or not User.authenticate_token(request.json.get("token")):
-        raise Unauthorized("Access token was not provided or is invalid", "Authentication")
+        return Unauthorized("Access token was not provided or is invalid", "Authentication")
 
 def require_same_user_as_email(f):
     @wraps(f)
+    @jwt_required()
     def view(*args, **kwargs):
-        require_token()
-        if kwargs.get("email") != User.authenticate_token(request.json.get("token")).email:
+        user = get_jwt_identity()
+        if kwargs.get("email") != user["email"]:
             raise Unauthorized("Access token is invalid", "Authentication")
         return f(*args, **kwargs)
     return view
@@ -20,9 +24,9 @@ def require_same_user_as_email(f):
 def require_same_user_as_group_id_user(f):
     """Verify that the user from the token is the owner of the group_id group"""
     @wraps(f)
-    def view(*args, **kwargs):
-        require_token()
-        
+    @jwt_required()
+
+    def view(*args, **kwargs):        
         if kwargs.get("id") and Group.get_by_id(kwargs["id"]).user_id != User.authenticate_token(request.json.get("token")).id:
             raise Unauthorized("You do not own this group", "Authentication")
         return f(*args, **kwargs)
