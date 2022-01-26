3b6718f10db4fbe148dfc7ead575828d1df68cfa
Julio Barrios <julio.junior02@gmail.com>
2022-01-25 18:36:30 -0600

    Add new property to Group_Payments

--- models/Group_Payments.py
+++ models/Group_Payments.py
@@ -17,6 +17,11 @@ class Group_Payments(BaseModel):
         db.ForeignKey("groups.id", ondelete="CASCADE"),
         primary_key=True
     )
+    member_id_paid = db.Column(
+        db.Integer(),
+        db.ForeignKey("group_members.id", ondelete="CASCADE"),
+        primary_key=True
+    )
     name: str = db.Column(
         db.String(45),
         nullable=False
@@ -34,4 +39,4 @@ class Group_Payments(BaseModel):
     member_payments: List[Member_Payments] = db.relationship("Member_Payments", backref="group_members_payments", passive_deletes=True)
 
     def __repr__(self):
-        return f'<Group_Payments id={self.id} group_id={self.group_id} name={self.name} total_amount={self.total_amount} created_on={self.created_on} member_payments={self.member_payments}>'
+        return f'<Group_Payments id={self.id} group_id={self.group_id} member_id_paid={self.member_id_paid} name={self.name} total_amount={self.total_amount} created_on={self.created_on} member_payments={self.member_payments}>'
