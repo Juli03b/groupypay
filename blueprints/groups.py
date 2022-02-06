2df03bc574b56b567a14603f49f8bb860aa64aa6
Julio Barrios <julio.junior02@gmail.com>
2022-02-05 22:39:19 -0600

    Add change route names

--- blueprints/groups.py
+++ blueprints/groups.py
@@ -4,6 +4,7 @@ from functools import wraps
 from flask import request
 from flask.blueprints import Blueprint
 from flask.json import jsonify
+from blueprints.helper import require_same_user_as_email, require_token
 from db_helpers.Group_Payment import Group_Payment
 from db_helpers.Member_Payment import Member_Payment
 from exceptions.Unauthorized import Unauthorized
@@ -15,7 +16,7 @@ from db_helpers.Group_Member import Group_Member
 
 groups_blueprint = Blueprint("groups", __name__)
 
-def require_token(f):
+def users_blueprint(f):
     @wraps(f)
     def decorated_function2(*args, **kwargs):
         if not request.json.get("token") or not User.authenticate_token(request.json.get("token")):
@@ -30,11 +31,32 @@ def require_same_id(f):
         return f(*args, **kwargs)
     return decorated_function1
 
-@groups_blueprint.get("/<id>")
-def get_group(id: str):
-    return jsonify(Group.get_by_id(id).__dict__)
+
+# @require_same_user_as_email
+@groups_blueprint.get("/", strict_slashes=False)
+def get_groups(email: str):
+    """Get a group"""
+    user = User.get_by_email(email)
+    print("GROUPS!!!!!!!!!!!!!!!!!", user.groups, "GROUPS!!!!!!!!!!!!!!!!!")
+    return jsonify(user.groups), 200
+
+@groups_blueprint.post("/", strict_slashes=False)
+@require_same_user_as_email
+def make_group(email: str):
+    """Make a group"""
+    validate_json(request.json, "group")
+    user = User.get_by_email(email)
+    group = user.make_group(request.json.get("name"), request.json.get("description"))
     
-@require_token
+    return jsonify(group.__dict__), 201
+
+@groups_blueprint.get("/<id>")
+def get_group(email, id: str):
+    user = User.get_by_email(email)
+    group = user.get_group(id)
+    return jsonify(group)
+    
+# @require_token
 @require_same_id
 @groups_blueprint.patch("/<id>")
 def patch_group(id: str):
@@ -61,17 +83,18 @@ def get_member(group_id: int, member_id: int):
 
     return jsonify(group_member.__dict__)
 
-@require_token
+# @require_token
 @require_same_id
 @groups_blueprint.post("/<group_id>/members")
-def add_member(group_id: int):
+def add_member(email: str, group_id: int):
     validate_json(request.json, "group_member")
-    group = Group.get_by_id(group_id)
+    user = User.get_by_email(email)
+    group = user.get_group(group_id)
     member = group.add_member(request.json.get("name"), request.json.get("email"), request.json.get("phone_number"))
     
     return jsonify(message="Successfully created group member", member=member.__dict__), 201
 
-@require_token
+# @require_token
 @require_same_id
 @groups_blueprint.patch("/<group_id>/members/<member_id>")
 def patch_group_member(group_id: int, member_id: int):
@@ -94,18 +117,18 @@ def get_group_payments(group_id: int):
 def get_group_payment(group_id: int, payment_id: int):
     group_payment = Group_Payment.get_by_id(payment_id, group_id)
     group_payment.total_amount = str(group_payment.total_amount)
-    return jsonify(group_payment.__dict__)
+    return jsonify(group_payment)
 
-@require_token
+# @require_token
 @require_same_id
 @groups_blueprint.post("/<group_id>/payments")
-def add_group_payment(group_id: int):
+def add_group_payment(email, group_id: int):
     """Add group payment"""
     print("!!!!!!!!!!!!!!!!!!!!!!!!!!", request.json, "!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     if not request.json or not request.json.get("name") or not  request.json.get("total_amount"):
         raise Bad_Request("Missing information")
-
-    group = Group.get_by_id(group_id)
+    user = User.get_by_email(email)
+    group = user.get_group(group_id)
     group_payment = group.add_payment(request.json["name"], request.json["total_amount"], request.json["member_id"])
     
     group_payment.add_member_payments(request.json["member_payments"])
@@ -113,7 +136,7 @@ def add_group_payment(group_id: int):
     
     return jsonify(group_payment)
 
-@require_token
+# @require_token
 @require_same_id
 @groups_blueprint.patch("/<group_id>/payments/<payment_id>")
 def patch_group_payment(group_id: int, payment_id: int):
@@ -138,7 +161,7 @@ def create_member_payments(group_id: int, payment_id: int):
 
 #! /groups/<group_id>/payments/<group_payment_id>/member_payments
 
-@groups_blueprint.get("/groups/<group_id>/payments/<group_payment_id>")
+@groups_blueprint.get("/<group_id>/payments/<group_payment_id>")
 def get_member_payments(group_id: int, group_payment_id: int):
     """Get a group payment's member payments"""
     group_payment = Group_Payment.get_by_id(group_payment_id, group_id)
@@ -147,23 +170,27 @@ def get_member_payments(group_id: int, group_payment_id: int):
 
 #! /groups/<group_id>/payments/<group_payment_id>/member_payments/members/<member_id>
 
-@groups_blueprint.get("/groups/<group_id>/payments/<group_payment_id>/member_payments/members/<member_id>")
+@groups_blueprint.get("/<group_id>/payments/<group_payment_id>/member_payments/members/<member_id>")
 def get_member_payment(group_payment_id: int, member_id):
     member_payment = Member_Payment.get_by_id(member_id, group_payment_id)
     
     return jsonify(member_payment=member_payment)
 
-@groups_blueprint.post("/groups/<group_id>/payments/<group_payment_id>/member_payments/members/<member_id>")
+@groups_blueprint.post("/<group_id>/payments/<group_payment_id>/member_payments/members/<member_id>")
 def make_member_payment(group_payment_id: int, member_id):
     member_payment = Member_Payment.get_by_id(member_id, group_payment_id)
     
     return jsonify(member_payment=member_payment)
 
 @groups_blueprint.post("/<group_id>/payments/<group_payment_id>/member-payments/<member_id>/pay", strict_slashes=False)
-def pay_payment(group_id, group_payment_id: int, member_id):
+def pay_payment(email, group_id, group_payment_id: int, member_id):
     """Pay processes"""
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    member_payment = Member_Payment.get_by_id(member_id, group_payment_id)
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", email, group_id, group_payment_id, member_id, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    user = User.get_by_email(email)
+    group = user.get_group(group_id)
+    payment = group.get_payment(group_payment_id)
+    
+    member_payment = payment.get_member_payment(member_id)
     member_payment.pay()
     
     return jsonify("Paid"), 200
