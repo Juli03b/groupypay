866c45c672b9e6c57f1173836dbcce44dc0c2f40
Julio Barrios <julio.junior02@gmail.com>
2022-02-05 22:38:20 -0600

    Remove unused code

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from flask.json import jsonify
 from os import environ
 from flask import Flask, request
 from flask_jwt_extended import JWTManager
-from functools import wraps
-from exceptions.Unauthorized import Unauthorized
 from models.main import connect_db, db
 from blueprints.users import users_blueprint
 from blueprints.auth import auth_blueprint
@@ -20,7 +18,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY', 'its_a_secret')
 app.config["JWT_SECRET_KEY"] = environ.get("JWT_SECRET_KEY", "secret-af")
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', "postgresql:///groupypay").replace("postgres://", "postgresql://", 1) 
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', "postgresql:///groupypay")
 
 CORS(app)
 JWTManager(app)
@@ -32,7 +30,7 @@ db.create_all()
 # Register blueprint for users routes
 app.register_blueprint(users_blueprint, url_prefix="/users")
 app.register_blueprint(auth_blueprint, url_prefix="/auth")
-app.register_blueprint(groups_blueprint, url_prefix="/groups")
+app.register_blueprint(groups_blueprint, url_prefix="/users/<email>/groups")
 
 @app.errorhandler(Custom_Error)
 def error_handler(error):
